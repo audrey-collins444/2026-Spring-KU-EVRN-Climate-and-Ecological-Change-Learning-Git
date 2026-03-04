5afe28a92bbed2f6238fef50ad14c1cd321753d5
Audrey Collins <a214c656@ku.edu>
2026-03-04 16:47:09 -0600

    Update address filter year

--- code/plot_keeling_curve_ggplot.R
+++ code/plot_keeling_curve_ggplot.R
@@ -1,8 +1,8 @@
 # an R script to use dplyr and ggplot to visualize
 # the Keeling curve of CO2 concentrations
 
-# Naupaka Zimmerman
-# nbzimmerman@ku.edu
+# Audrey Collins
+# a214c656@ku.edu
 # February 25, 2026
 
 # first step is load the packages we want to use
@@ -15,7 +15,7 @@ mlo_data <- read.csv("data/monthly_in_situ_co2_mlo_cleaned.csv")
 # use a dplyr pipe and filter function to get rid of -99 values
 mlo_data %>%
   filter(CO2 > 0) %>%
-  filter(Yr > 1985) %>%
+  filter(Yr > 1990) %>%
   ggplot(aes(x = Date.1,
              y = CO2)) +
   geom_line()
